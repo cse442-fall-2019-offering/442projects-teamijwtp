f59f494b026e73205c8de5aca866050f514d244c
TahaAlam <tahaalam@buffalo.edu>
2019-10-18 13:49:14 -0400

    Add files via upload
    Modified home page to show example of Event Component working.

--- App.js
+++ App.js
@@ -5,19 +5,42 @@ import HomeButton from './HomeButton';
 import ChatButton from './ChatButton';
 import SettingButton from './SettingButton';
 import MenuItem from './components/MenuItem';
+import EventTable from './components/EventTable';
 
-export default function App() {
+export default function App()  {
+    this.SampleNameArray = [
+      'Goa',
+      'Gujrat',
+      'Madhya Pradesh',
+      'Assam',
+      'Gujrat',
+      'Karnataka',
+      'Jharkhand',
+      'Himachal Pradesh',
+      'Manipur',
+      'Meghalaya',
+      'Mizoram',
+      'Uttarakhand',
+      'West Bengal',
+      'Tamil Nadu ',
+      'Punjab',
+      'Rajasthan',
+      'Bihar',
+      'Andhra Pradesh',
+      'Arunachal Pradesh',
+    ];
   const [value, onChangeText] = React.useState('Enter Group Code');
   return (
     
     <View style={styles.container}>
+   {/*
     <Text style={styles.normText}> Feed will go here </Text>
       <TextInput
       style={styles.revText}
       onChangeText={text => onChangeText(text)}
       value={value}
        />
-        
+      
         <View style={styles.menu}> 
           <MenuItem itemImage={require('./icons/eat.png')} fnc={()=>alert("food")} />
           <MenuItem itemImage={require('./icons/gym.png')} fnc={()=>alert("gym")} />
@@ -25,9 +48,10 @@ export default function App() {
           <MenuItem itemImage={require('./icons/study.png')} fnc={()=>alert("study")} />
           <MenuItem itemImage={require('./icons/shop.png')} fnc={()=>alert("shop")}/>
           <MenuItem itemImage={require('./icons/other.png')} fnc={()=>alert("other")}/>
+        </View>*/}
+        <View style ={styles.testing}>
+          <EventTable eventDescription={"Des"} eventLocation={"Loc"} eventTime={"SampleTime"} eventNames={SampleNameArray} />
         </View>
-        
-        
         
         <View style={ styles.bottomView} >
                   <HomeButton />
@@ -58,7 +82,9 @@ const styles = StyleSheet.create({
     flexDirection:'row',
     flexWrap: 'wrap',
   },
-
+  testing:{
+    height:'20%',
+  },
   bottomView:{
     width: '100%',
     height: 50,
@@ -68,8 +94,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     position: 'absolute',
     bottom: 0,
-
-
   },
 
   normText: { 
